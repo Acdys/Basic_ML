1d9cf6d21957335139ca73a86c4f083c03343a80
Елизавета Величко <117599789+Acdys@users.noreply.github.com>
2025-06-01 16:33:26 +0300

    Create _init_.py

---
+++ homework_06/_init_.py
@@ -0,0 +1,9 @@
+"""
+Домашнее задание №2
+Классы и модули
+"""
+from homework_06.exceptions import LowFuelError, NotEnoughFuel
+from homework_06.base import Vehicle
+from homework_06.car import Car
+from homework_06.engine import Engine
+from homework_06.plane import Plane
